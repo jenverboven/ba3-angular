fb2e18f1b5c9e3c37d8e37e47d312159e393fba3
jenverboven <r0889629@student.thomasmore.be>
2023-09-16 00:58:41 +0200

    voorbeelden tot en met how to navigate between components af

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // ArticleComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
