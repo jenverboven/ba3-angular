b28df9da2ffa690a511432bc7dc5a966c15f539c
jenverboven <r0889629@student.thomasmore.be>
2023-09-17 15:01:06 +0200

    voorbeelden klaar tot en met interface, service en component interaction

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Article } from '../article';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
 
+export class ArticleComponent implements OnInit {
+  @Input() article: Article = { id: 0, title: "", subtitle: "", imageUrl: "", imageCaption: "", content: "", author: "", publishDate: "" };
+
+  constructor() {  }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../article';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+
+  articles: Article[] = [];
+
+  constructor(private articleService: ArticleService) {  }
+
+  ngOnInit(): void {
+    this.articles = this.articleService.getArticles();
+  }
 }
