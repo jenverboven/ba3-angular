c1096af34a1f26eb8a7b488f4320468e00bee56c
jenverboven <r0889629@student.thomasmore.be>
2023-09-25 14:45:01 +0200

    tot en met responsive navbar klaar

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
 export class MenuComponent {
 
+  constructor(private router: Router){  }
+
+  ngOnInit(): void{
+
+  }
+
+  hamburgerOpen = false;
+
+  toggleHamburger(): void {
+    this.hamburgerOpen = !this.hamburgerOpen;
+  }
+
+  onHamburgerItemClick() {
+    if (this.hamburgerOpen) {
+      this.hamburgerOpen = false;
+    }
+  }
+
+  navigateTo(path: string) {
+    this.hamburgerOpen = false;
+    this.router.navigate([path])
+  }
 }
